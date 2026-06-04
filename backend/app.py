599abb2263c5647dd8117a1215de50249a21659d
vidhatrihr <167363972+vidhatrihr@users.noreply.github.com>
2026-06-04 14:02:30 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,6 @@ from populate_db import populate_db
 
 app = Flask(__name__)
 app.secret_key = 'super-secret-key'
-
 app.config.update(
     SESSION_COOKIE_SAMESITE='None',
     SESSION_COOKIE_SECURE=True,
@@ -18,19 +17,21 @@ app.config.update(
 CORS(app, supports_credentials=True, origins=['http://localhost:5173'])
 
 db.init_app(app)
-
-login_manager = LoginManager(app)
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+with app.app_context():
+  db.create_all()
+  populate_db()
 
 app.register_blueprint(auth_bp, url_prefix='/api')
 app.register_blueprint(blogs_bp, url_prefix='/api')
 
-with app.app_context():
-    db.create_all()
-    populate_db()
+
+login_manager = LoginManager(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(int(user_id))
+
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+  app.run(debug=True)
